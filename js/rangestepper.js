7f9e7f9994acfd203546c3b69815fa0785672aff
Radu-Sebastian <radu@findie.me>
2015-01-07 17:32:26 +0200

    Added hidden input for form submit compatibility

--- js/rangestepper.js
+++ js/rangestepper.js
@@ -8,18 +8,24 @@
 
 (function ($) {
 
-    $.fn.rangestepper = function (minval, maxval) {
+    $.fn.rangestepper = function ( options ) {
 
+        var settings = $.extend({
+            inputName: "rangestepper",
+            minVal: 0,
+            maxVal: 10
+        }, options );
+        
         //If it doesn't have the rangestepper class, add it.
         if (!this.hasClass('rangestepper')) {
             this.addClass('rangestepper');
         }
-        //Check if minval and maxval are integers
-        if (!(minval === parseInt(minval, 10)) || !(maxval === parseInt(maxval, 10)))
+        //Check if settings.minVal and settings.maxVal are integers
+        if (!(settings.minVal === parseInt(settings.minVal, 10)) || !(settings.maxVal === parseInt(settings.maxVal, 10)))
             return;
 
         //Compute the number of steps required
-        var steps = maxval - minval;
+        var steps = settings.maxVal - settings.minVal;
 
         //If there aren't any steps, exit.
         if (steps <= 0)
@@ -27,13 +33,13 @@
 
         //Create the actual steps and their value
         for (var i = 0; i <= steps; i++) {
-            var newstep = $("<div class='step' data-val='" + (minval + i) + "' />");
+            var newstep = $("<div class='step' data-val='" + (settings.minVal + i) + "' />");
             this.append(newstep);
 
             //We need a space to center them with variable space between.
             this.append(' ');
         }
-        this.append('<span class="stretch"></span><div class="rangeline"></div>');
+        this.append('<span class="stretch"></span><div class="rangeline"></div><input type="hidden" name="' + settings.inputName + '" />');
 
         $(document).on('click', '.rangestepper .step', function () {
             if( !$(this).children('.dragger').length ){
@@ -45,7 +51,8 @@
                 $(this).html("<div class='dragger'><div class='arrow'>&#8801;</div><div class='active'></div></div>");
 
                 //Update the current value
-                $(this).parent().data('val', $(this).data('val'));
+                $('.rangestepper input[name="' + settings.inputName + '"]').val($(this).data('val'));
+
             }
         });
 
@@ -85,6 +92,7 @@
             });
             $('.dragger').remove();
             closestSnap.html("<div class='dragger'><div class='arrow'>&#8801;</div><div class='active'></div></div>");
+            $('.rangestepper input[name="' + settings.inputName + '"]').val(closestSnap.data('val'));
 
         });
 
